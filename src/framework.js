579142a8c7d368ec563256f1335a99f306c8e9bb
Wilrey <wilrey@gmail.com>
2026-05-05 15:36:46 +0200

    Update framework.js

--- src/framework.js
+++ src/framework.js
@@ -2,7 +2,7 @@ var contactSearchCallback;
 
 window.Framework = {
     config: {
-        name:"ExampleGitHubApp",
+        name: "ExampleGitHubApp",
         clientIds: {
             'mypurecloud.de': 'ee4696b8-16c4-40ce-87ae-7322597880ac'
         },
@@ -26,63 +26,82 @@ window.Framework = {
     initialSetup: function () {
         window.PureCloud.subscribe([
             {
-                type: 'Interaction', 
+                type: 'Interaction',
                 callback: function (category, interaction) {
-                    window.parent.postMessage(JSON.stringify({type:"interactionSubscription", data:{category:category, interaction:interaction}}) , "*");
-                }  
+                    window.parent.postMessage(JSON.stringify({
+                        type: "interactionSubscription",
+                        data: { category: category, interaction: interaction }
+                    }), "*");
+                }
             },
             {
-                type: 'UserAction', 
+                type: 'UserAction',
                 callback: function (category, data) {
-                    window.parent.postMessage(JSON.stringify({type:"userActionSubscription", data:{category:category, data:data}}) , "*");
-                }  
+                    window.parent.postMessage(JSON.stringify({
+                        type: "userActionSubscription",
+                        data: { category: category, data: data }
+                    }), "*");
+                }
             },
             {
-                type: 'Notification', 
+                type: 'Notification',
                 callback: function (category, data) {
-                    window.parent.postMessage(JSON.stringify({type:"notificationSubscription", data:{category:category, data:data}}) , "*");
-                }  
+                    window.parent.postMessage(JSON.stringify({
+                        type: "notificationSubscription",
+                        data: { category: category, data: data }
+                    }), "*");
+                }
             }
         ]);
 
-        window.addEventListener("message", function(event) {
+        window.addEventListener("message", function (event) {
             try {
                 var message = JSON.parse(event.data);
-                if(message){
-                    if(message.type == "clickToDial"){
+                if (message) {
+                    console.log(message.data);
+                    if (message.type == "clickToDial") {
                         window.PureCloud.clickToDial(message.data);
-                    } else if(message.type == "addAssociation"){
+                    } else if (message.type == "addAssociation") {
                         window.PureCloud.addAssociation(message.data);
-                    }else if(message.type == "addAttribute"){
+                    } else if (message.type == "addAttribute") {
                         window.PureCloud.addCustomAttributes(message.data);
-                    }else if(message.type == "addTransferContext"){
+                    } else if (message.type == "addTransferContext") {
                         window.PureCloud.addTransferContext(message.data);
-                    }else if(message.type == "sendContactSearch"){
-                        if(contactSearchCallback) {
+                    } else if (message.type == "sendContactSearch") {
+                        if (contactSearchCallback) {
                             contactSearchCallback(message.data);
                         }
-                    }else if(message.type == "updateUserStatus"){
+                    } else if (message.type == "updateUserStatus") {
                         window.PureCloud.User.updateStatus(message.data);
-                    }else if(message.type == "updateInteractionState"){
+                    } else if (message.type == "updateInteractionState") {
                         window.PureCloud.Interaction.updateState(message.data);
-                    }else if(message.type == "setView"){
+                    } else if (message.type == "setView") {
                         window.PureCloud.User.setView(message.data);
-                    }else if(message.type == "updateAudioConfiguration"){
+                    } else if (message.type == "updateAudioConfiguration") {
                         window.PureCloud.User.Notification.setAudioConfiguration(message.data);
-                    }else if(message.type == "sendCustomNotification"){
+                    } else if (message.type == "sendCustomNotification") {
                         window.PureCloud.User.Notification.notifyUser(message.data);
                     }
                 }
             } catch {
-                //ignore if you can not parse the payload into JSON
+                // ignore if you can not parse the payload into JSON
             }
         });
     },
+
     screenPop: function (searchString, interaction) {
-        window.parent.postMessage(JSON.stringify({type:"screenPop", data:{searchString:searchString, interactionId:interaction}}) , "*");
+        window.parent.postMessage(JSON.stringify({
+            type: "screenPop",
+            data: { searchString: searchString, interactionId: interaction }
+        }), "*");
     },
+
     processCallLog: function (callLog, interaction, eventName, onSuccess, onFailure) {
-        window.parent.postMessage(JSON.stringify({type:"processCallLog" , data:{callLog:callLog, interactionId:interaction, eventName:eventName}}) , "*");
+        window.parent.postMessage(JSON.stringify({
+            type: "processCallLog",
+            data: { callLog: callLog, interactionId: interaction, eventName: eventName }
+        }), "*");
+
         var success = true;
         if (success) {
             onSuccess({
@@ -92,11 +111,19 @@ window.Framework = {
             onFailure();
         }
     },
-    openCallLog: function(callLog, interaction){
-        window.parent.postMessage(JSON.stringify({type:"openCallLog" , data:{callLog:callLog, interaction:interaction}}) , "*");
+
+    openCallLog: function (callLog, interaction) {
+        window.parent.postMessage(JSON.stringify({
+            type: "openCallLog",
+            data: { callLog: callLog, interaction: interaction }
+        }), "*");
     },
-    contactSearch: function(searchString, onSuccess, onFailure) {
+
+    contactSearch: function (searchString, onSuccess, onFailure) {
         contactSearchCallback = onSuccess;
-        window.parent.postMessage(JSON.stringify({type:"contactSearch" , data:{searchString:searchString}}) , "*");
+        window.parent.postMessage(JSON.stringify({
+            type: "contactSearch",
+            data: { searchString: searchString }
+        }), "*");
     }
 };
